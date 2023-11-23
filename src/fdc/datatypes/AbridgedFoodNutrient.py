a2a5a063f267e4ee980491a57dd9fb10aff5d7fd
ghostiek <patricktawil1@hotmail.com>
2023-11-23 02:53:02 -0500

    Added docstrings and type annotations for AbridgedFood and AbridgedFoodNutrients

--- src/fdc/datatypes/AbridgedFoodNutrient.py
+++ src/fdc/datatypes/AbridgedFoodNutrient.py
@@ -2,7 +2,18 @@ from .Nutrient import Nutrient
 
 
 class AbridgedFoodNutrient:
-    def __init__(self,number, name, amount, unit_name, derivation_code, derivation_description):
+    def __init__(self, number: int, name: str, amount: float, unit_name: str, derivation_code: str,
+                 derivation_description: str):
+        """
+        Abridged Food Nutrient
+        :param number: ID of Nutrient
+        :param name: Name of Nutrient
+        :param amount: Quantity of Nutrient
+        :param unit_name: Unit of Nutrient
+        :param derivation_code: Code that represents the derivation of how the amount was calculated. exmaple: LCCD
+        :param derivation_description: Description of Derivation Code. Example: Calculated from a daily value percentage
+        per serving size measure
+        """
         self.number = number
         self.name = name
         self.amount = amount
