d2e259f4984489a2bca53eb6237bf1c9510847f4
ghostiek <patricktawil1@hotmail.com>
2023-11-24 19:39:39 -0500

    Added FoodAttribute and FoodPortion classes

---
+++ src/fdc/datatypes/FoodAttributeType.py
@@ -0,0 +1,5 @@
+class FoodAttributeType:
+    def __init__(self, id: int = None, name: str = None, description: str = None):
+        self.id = id
+        self.name = name
+        self.description = description
--- src/fdc/datatypes/SampleFood.py
+++ src/fdc/datatypes/SampleFood.py
@@ -2,25 +2,21 @@ from datetime import datetime
 from .FoodComponent import FoodComponent
 from .FoodNutrient import FoodNutrient
 from .FoodCategory import FoodCategory
+from .FoodAttribute import FoodAttribute
+from .FoodPortion import FoodPortion
 
 
 class SampleFood:
-    def __init__(self, fdc_id: int, data_type: str, description: str, food_class: str = None, store_city: str = None,
+    def __init__(self, fdc_id: int, data_type: str, description: str, food_class: str = None,
                  food_attributes: list = None, food_nutrients: str = None, food_portions: list = None,
-                 brand_description: str = None, sample_lot_number: str = None, food_components: list = None,
-                 store_name: str = None, store_state: str = None, upc_code: str = None,
-                 food_category: dict = None, publication_date: str = None):
+                 food_components: list = None, food_category: dict = None, publication_date: str = None):
         self.fdc_id = fdc_id
         self.description = description
         self.publication_date = datetime.strptime(publication_date, "%m/%d/%Y").date()
         self.data_type = data_type
         self.food_class = food_class
         self.food_components = [FoodComponent(**i) for i in food_components] if food_components else food_components
-        self.food_attributes = food_attributes
-        if food_attributes:
-            print("WARNING: Class FoodAttribute was not created because it has yet to be encountered")
+        self.food_attributes = [FoodAttribute(**food_attribute) for food_attribute in food_attributes] if food_attributes else food_attributes
         self.food_nutrients = [FoodNutrient(**i) for i in food_nutrients] if food_nutrients else food_nutrients
-        self.food_portions = food_portions
-        if food_portions:
-            print("WARNING: Class FoodPortions was not created because it has yet to be encountered")
+        self.food_portions = [FoodPortion(**food_portion) for food_portion in food_portions] if food_portions else food_portions
         self.food_category = FoodCategory(**food_category) if food_category else food_category
